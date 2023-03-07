95ca18e1b1b4e3ba9c80a15de68cf4424b3c2565
jiechen257 <1487503910@qq.com>
2023-03-07 16:59:41 +0800

    init

---
+++ pages/index/home/components/home-swiper.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<swiper class="swiper" circular indicator-active-color="#fff" indicator-color="rgba(255,255,255,0.6)"
+			:indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item class="swiper-item flexr-jsc flex-aic">
+				<image mode="aspectFill" class="swiper-item-img"
+					src="https://via.placeholder.com/200x100.png/FFB6C1">
+				</image>
+			</swiper-item>
+			<swiper-item class="swiper-item flexr-jsc flex-aic">
+				<image mode="aspectFill" class="swiper-item-img"
+					src="https://via.placeholder.com/200x100.png/FFB6C1">
+				</image>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+.swiper {
+		height: 304rpx;
+		.swiper-item {
+			width: 100%;
+			height: 100%;
+		}
+		.swiper-item-img {
+			width: 686rpx;
+			height: 100%;
+		}
+	}
+</style>
---
+++ pages/index/home/home.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<home-search></home-search>
+		<home-swiper></home-swiper>
+		<home-grid></home-grid>
+<!--		<image class="bar_box" src="http://www.liwanying.top/applate-icon/bar.png" mode="widthFix"></image>-->
+		<view class="tabs_box">
+			<lyz-tabs type="scroll" v-model="tabIndex" @change="(item) => getTab(item.id)" :columns="[{title:'ALL',id:'4'},{title:'NIKE',id:'5'},{title:'adidas',id:'3'},{title:'Converse',id:'6'},{title:'ERKE',id:'7'}]"></lyz-tabs>
+		</view>
+		<home-flow></home-flow>
+	</view>
+</template>
+
+<script setup>
+	import homeSearch from './components/home-search.vue'
+	import homeSwiper from './components/home-swiper.vue'
+	import homeGrid from './components/home-grid.vue'
+	import homeFlow from './components/home-flow.vue'
+	import {ref} from 'vue'
+	let tabIndex = ref(0)
+	const getTab = (id) => {
+		console.log(id)
+	}
+</script>
+
+<style lang="scss" scoped>
+.bar_box {
+	width: 100%;
+	height: 140rpx;
+}
+.tabs_box {
+	padding: 24rpx 0;
+}
+</style>
