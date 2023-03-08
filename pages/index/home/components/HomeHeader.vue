ef3f634fd7fdf789618a6e930beff3f4a62fb249
chenjie <1487503910@qq.com>
2023-03-08 21:25:54 +0800

    feat: 增加首页顶部导航栏

--- pages/index/home/components/HomeHeader.vue
+++ pages/index/home/components/HomeHeader.vue
@@ -1,6 +1,14 @@
 <template>
   <view class="bg-color">
-    <uni-nav-bar left-icon="left" right-icon="cart" title="标题" />
+    <!-- <uni-nav-bar left-icon="left" right-icon="cart" title="标题" /> -->
+    <uni-nav-bar
+      left-icon="bars"
+      right-icon="shop"
+      title="SHOPPAY"
+      color="#fff"
+      backgroundColor="#1E385A"
+      :border="false"
+    />
   </view>
 </template>
 
