b8aaa55dff0e7ddf9a7066073e4f4f6b49cd61fc
jiechen257 <1487503910@qq.com>
2023-03-31 17:36:34 +0800

    chore: change dependencies

--- pages/index/home/components/home-grid.vue
+++ pages/index/home/components/home-grid.vue
@@ -1,37 +1,43 @@
 <template>
-  <swiper
-    class="swiper"
-    :style="{
-      height: 4 <= 4 ? '170rpx' : '360rpx',
-    }"
-  >
-    <swiper-item class="swiper-item">
-      <lyz-grid
-        @tapGrid="tapGrid"
-        :border="false"
-        :columnNum="4"
-        :columns="[
-          {
-            title: 'Shoes',
-            icon: 'http://www.liwanying.top/applate-icon/muying2x.png',
-          },
-          {
-            title: 'T-Shirt',
-            icon: 'http://www.liwanying.top/applate-icon/muying2x.png',
-          },
-          {
-            title: 'Tops',
-            icon: 'http://www.liwanying.top/applate-icon/muying2x.png',
-          },
-          {
-            title: 'Kid',
-            icon: 'http://www.liwanying.top/applate-icon/muying2x.png',
-          },
-        ]"
-      >
-      </lyz-grid>
-    </swiper-item>
-  </swiper>
+  <view>
+    <view class="header">
+      <view class="header-left"> CATEGORIES </view>
+      <view class="header-right"> See All </view>
+    </view>
+    <!--    <swiper-->
+    <!--      class="swiper"-->
+    <!--      :style="{-->
+    <!--        height: 4 <= 4 ? '170rpx' : '360rpx',-->
+    <!--      }"-->
+    <!--    >-->
+    <!--      <swiper-item class="swiper-item">-->
+    <!--        <lyz-grid-->
+    <!--          @tapGrid="tapGrid"-->
+    <!--          :border="false"-->
+    <!--          :columnNum="4"-->
+    <!--          :columns="[-->
+    <!--            {-->
+    <!--              title: 'Shoes',-->
+    <!--              icon: 'http://www.liwanying.top/applate-icon/muying2x.png',-->
+    <!--            },-->
+    <!--            {-->
+    <!--              title: 'T-Shirt',-->
+    <!--              icon: 'http://www.liwanying.top/applate-icon/muying2x.png',-->
+    <!--            },-->
+    <!--            {-->
+    <!--              title: 'Tops',-->
+    <!--              icon: 'http://www.liwanying.top/applate-icon/muying2x.png',-->
+    <!--            },-->
+    <!--            {-->
+    <!--              title: 'Kid',-->
+    <!--              icon: 'http://www.liwanying.top/applate-icon/muying2x.png',-->
+    <!--            },-->
+    <!--          ]"-->
+    <!--        >-->
+    <!--        </lyz-grid>-->
+    <!--      </swiper-item>-->
+    <!--    </swiper>-->
+  </view>
 </template>
 
 <script setup>
@@ -43,4 +49,22 @@
   }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 10upx;
+    padding: 20upx 30upx;
+  }
+  .header-left {
+    color: #000;
+    font-size: 32upx;
+    font-weight: 600;
+  }
+  .header-right {
+    color: #de6053;
+    font-size: 28upx;
+    font-weight: 600;
+  }
+</style>
--- pages/index/home/components/home-header.vue
+++ pages/index/home/components/home-header.vue
@@ -1,14 +1,15 @@
 <template>
   <view class="bg-color">
-    <!-- <uni-nav-bar left-icon="left" right-icon="cart" title="标题" /> -->
-    <!--    <uni-nav-bar-->
-    <!--      left-icon="bars"-->
-    <!--      right-icon="shop"-->
-    <!--      title="SHOPPAY"-->
-    <!--      color="#fff"-->
-    <!--      backgroundColor="#1E385A"-->
-    <!--      :border="false"-->
-    <!--    />-->
+    <!--    <uni-nav-bar left-icon="left" right-icon="cart" title="标题" />-->
+    <uni-nav-bar
+      fixed
+      left-icon="bars"
+      right-icon="shop"
+      title="NOVA"
+      color="#fff"
+      backgroundColor="#1E385A"
+      :border="false"
+    />
     <view>
       <Vue3Lottie :animationData="animationData" :height="200" :width="200" />
     </view>
