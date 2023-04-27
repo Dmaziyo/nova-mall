6dff1d118d7124440b84f902b5292aa9e11a9201
D城兔Maziyo <247948681@qq.com>
2023-04-27 19:35:12 +0800

    refactor: correct the style of file name

--- pages/tabbar/goodsCategory/components/category-header.vue
+++ pages/tabbar/goodsCategory/components/category-header.vue
@@ -1,105 +1,40 @@
 <template>
-  <view class="route-header">
-    <!-- <category-route
-      @iconClick="clickHandler"
-      v-for="item in list"
-      :iconType="item.iconType"
-      :text="item.text"
-      :key="item.text"
-      :activeType="item.activeType"
-      :checked="item.text === current"
-    ></category-route> -->
-    <view v-for="item in routeList" class="category-link" :key="item.text">
-      <view class="icon-box">
-        <uni-icons
-          @click="clickHandler(item.text, item.url)"
-          style="font-size: 30px"
-          class="back-btn iconfont"
-          :type="item.text === current ? item.activeType : item.iconType"
-        />
-      </view>
-      <view>{{ item.text }}</view>
-    </view>
+  <view class="search_box">
+    <uni-icons
+      style="font-size: 30px"
+      class="back-btn iconfont iconarrow_btn_left"
+      type="arrowleft"
+      @click="navBack"
+    />
+    <uni-search-bar placeholder="Search brand, products..." cancelButton="none"></uni-search-bar>
+    <uni-icons style="font-size: 30px" class="back-btn iconfont shop-filled" type="shop-filled" />
   </view>
 </template>
 
 <script>
   export default {
-    props: {
-      routeList: Array,
-    },
     methods: {
       // 返回上一页
-      clickHandler(text, url) {
-        console.log(url)
-        this.current = text
+      navBack() {
+        uni.switchTab({
+          url: '/',
+        })
       },
-    },
-    data() {
-      return {
-        list: [
-          {
-            iconType: 'shop',
-            activeType: 'shop-filled',
-            text: '全部',
-            url: 'link',
-          },
-          {
-            iconType: 'gift',
-            activeType: 'gift-filled',
-            text: '营养健康',
-            url: 'link',
-          },
-          {
-            iconType: 'star',
-            activeType: 'starhalf',
-            text: '口服美容',
-            url: 'link',
-          },
-          {
-            iconType: 'navigate',
-            activeType: 'navigate-filled',
-            text: '美妆护肤',
-            url: 'link',
-          },
-          {
-            iconType: 'camera',
-            activeType: 'camera-filled',
-            text: '母婴亲子',
-            url: 'link',
-          },
-          {
-            iconType: 'gear',
-            activeType: 'gear-filled',
-            text: '电子设备',
-            url: 'link',
-          },
-        ],
-        current: '全部',
-      }
     },
     // other options and methods
   }
 </script>
 
 <style lang="scss">
-  .route-header {
-    padding: 15px;
+  .search_box {
     display: flex;
-    column-gap: 30px;
-    overflow-x: scroll;
-  }
-  .category-link {
-    font-size: 12px;
-    white-space: nowrap;
-    .icon-box {
-      background-color: #f7f7f7;
-      padding: 8px;
-      border-radius: 50%;
-    }
-    display: flex;
-    flex-direction: column;
     align-items: center;
-    row-gap: 5px;
+    justify-content: space-between;
+  }
+  :deep(.uni-searchbar) {
+    flex: 1;
+  }
+  :deep(uni-icons) {
+    font-size: 35px;
   }
 </style>
